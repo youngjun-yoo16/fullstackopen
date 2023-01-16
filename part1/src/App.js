8867b3fd4ab18f5dfb92e2bf6029f1d40fe8edcc
Youngjun Yoo <visualjoon0916@gmail.com>
2023-01-16 08:02:36 +0000

    Done till component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const now = new Date()
+	const a = 10
+	const b = 20
+	console.log(now, a + b)
+	
+	return (
+		<div>
+			<p>Hello world, it is {now.toString()}</p>
+			<p>
+				{a} plus {b} is {a + b}
+			</p>
+		</div>
+	)
 }
 
-export default App;
+export default App 
