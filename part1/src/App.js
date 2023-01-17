76755e426f07c9969dad139150ec7fa16129d594
youngjun-yoo16 <yoo156@purdue.edu>
2023-01-17 14:13:07 +0000

    Finished with JSX, Multiple Components, and Props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,23 @@
+const Hello = (props) => {
+	console.log(props)
+	return (
+		<div>
+			<p>
+				Hello {props.name}, you are {props.age} years old
+			</p>
+		</div>
+	)
+}
+
 const App = () => {
-	const now = new Date()
-	const a = 10
-	const b = 20
-	console.log(now, a + b)
+	const name = 'Peter'
+	const age = 10
 	
 	return (
 		<div>
-			<p>Hello world, it is {now.toString()}</p>
-			<p>
-				{a} minus {b} is {a - b}
-			</p>
+			<h1>Greetings</h1>
+			<Hello name='Maya' age={26 + 10} />
+			<Hello name={name} age={age} />
 		</div>
 	)
 }
