cd37561da190644d30b3346c5124dc46ac15e01f
youngjun-yoo16 <yoo156@purdue.edu>
2023-02-12 13:25:39 +0000

    A more complex state, debugging React apps

--- part1/courseinfo/src/Test.js
+++ part1/courseinfo/src/Test.js
@@ -8,39 +8,19 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 
 const Test = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)  
-  
-  const increaseByOne = () => {
-		console.log('increasing, value before: ', counter)
-		setCounter(counter + 1)
-  }
-  
-  const decreaseByOne = () => {
-	  	console.log('decreasing, value before: ', counter)
-	  	setCounter(counter - 1)
-  }
-  
-  const setToZero = () => {
-	  	console.log('resetting to zero, value before: ', counter)
-	  	setCounter(0)
-  }
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)  
   
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />  
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+        left
+      </button>
+      <button onClick={() => setRight(right + 1)}>
+        right
+      </button>
+      {right}
     </div>
   )
 }
