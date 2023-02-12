10ad41f59282a40cb0f0845befe174dad157afb2
youngjun-yoo16 <yoo156@purdue.edu>
2023-02-12 13:51:37 +0000

    Hanling arrays

--- part1/courseinfo/src/Test.js
+++ part1/courseinfo/src/Test.js
@@ -8,24 +8,27 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 
 const Test = () => {
-  const [ clicks, setClicks ] = useState({
-	  left: 0, right: 0
-  })
-  
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
   const handleLeftClick = () => {
-	  setClicks({...clicks, left: clicks.left + 1})
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
-  
+
   const handleRightClick = () => {
-	  setClicks({...clicks, right: clicks.right + 1})
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
   
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
