e653fa7004890880cd247796c822635f10af720b
youngjun-yoo16 <yoo156@purdue.edu>
2023-02-14 14:34:21 +0000

    Passing Even Handlers to Child Components

--- part1/courseinfo/src/Test2.js
+++ part1/courseinfo/src/Test2.js
@@ -1,19 +1,24 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const Test2 = () => {
   const [value, setValue] = useState(10)
   
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // print the new value to console
+  const setToValue = (newValue) => {
     setValue(newValue)
   }
   
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
