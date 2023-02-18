3929dec26b2fb79255f473e175836ca361268947
youngjun-yoo16 <yoo156@purdue.edu>
2023-02-18 17:57:37 +0000

    Finished 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -24,6 +24,21 @@ const App = () => {
 	  setBad(bad + 1)
   }
   
+  const all = good + neutral + bad
+  const totalScore = good * 1 + bad * -1
+  const average = () => {
+	  if (all === 0) {
+		  return 0
+	  }
+	  return totalScore / all
+  }
+  const positive = () => {
+	  if (all === 0) {
+		  return 0
+	  }
+	  return (good / all) * 100
+  } 
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -34,6 +49,9 @@ const App = () => {
 	  <p>good {good}</p>
 	  <p>neutral {neutral}</p>
 	  <p>bad {bad}</p>
+	  <p>all {all}</p>
+	  <p>average {average()}</p>
+	  <p>positive {positive()} %</p>
     </div>
   )
 }
