ccce64b6d460338051feaf1643fb6d28dee11030
youngjun-yoo16 <yoo156@purdue.edu>
2023-02-23 03:57:24 +0000

    For submission

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import Test from './Test';
 import Test2 from './Test2';
 
-//ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 //ReactDOM.createRoot(document.getElementById('root')).render(<Test />);
-ReactDOM.createRoot(document.getElementById('root')).render(<Test2 />);
+//ReactDOM.createRoot(document.getElementById('root')).render(<Test2 />);
 
