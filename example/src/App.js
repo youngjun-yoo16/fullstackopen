46246f3eefe6a72cb7ab3c189de8d79220914ad7
youngjun-yoo16 <yoo156@purdue.edu>
2023-02-26 14:14:36 +0000

    Controlled component

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,43 @@ import Note from './components/Note'
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
-
+  const [newNote, setNewNote] = useState(
+	/* The placeholder text stored as the initial value of the newNote state appears
+  	in the input element. */
+    'a new note...' 
+  ) 
+  
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+	  
+	/* Creating a new object for the note called noteObject that will receive its
+	content from the component's newNote state. */
+	const noteObject = {
+    	content: newNote,
+    	important: Math.random() < 0.5,
+    	id: notes.length + 1,
+  	}
+	
+	/* The new note is added to the list of notes using the concat array method. 
+	This method does not mutate the original notes array, but rather creates a new
+	copy of the array with the new item added to the end. */
+	setNotes(notes.concat(noteObject))
+	
+	/* The event handler also resets the value of the controlled input element by
+	calling the setNewNote function of the newNote state. */
+  	setNewNote('')
+  }
+  
+  /* The event handler is called every time a change occurs in the input element. 
+  The event handler function receives the event object as its event parameter.
+  
+  The target property of the event object now corresponds to the controlled 
+  input element, and event.target.value refers to the input value of that element.
+  
+  Now the App component's newNote state reflects the current value of the input. */
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
   
   return (
@@ -18,7 +51,10 @@ const App = (props) => {
 		)}
       </ul>
       <form onSubmit={addNote}>
-        <input />
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
       </form>  	
     </div>
