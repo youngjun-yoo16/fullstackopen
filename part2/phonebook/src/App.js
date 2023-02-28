56f4997920edef3ba04ee30cafa14eecd5473e57
youngjun-yoo16 <yoo156@purdue.edu>
2023-02-28 13:51:31 +0000

    Finished 2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 
-const Name = ({ name }) => <li>{name}</li>
+const Name = ({ person }) => <li>{person.name} {person.number}</li>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
   const checkDuplicateName = () => {
 	   let find = false
 	   
 	   /* The find() method returns the first element in the provided array 
 	   that satisfies the provided testing function. If no values satisfy the 
-	   testing function, undefined is returned. */
+	   testing function, undefined is returned. 
+	   
+	   JS String Comparision using localCompare: https://www.freecodecamp.org/news/javascript-string-comparison-how-to-compare-strings-in-js/ */
 	   const result = persons.find(({ name }) => name.localeCompare(newName) === 0)
 	   
 	   /* result not equal to undefined means that there is a name that already
@@ -26,17 +29,20 @@ const App = () => {
 	   return find
   }
   
-  const addName = (event) => {
+  const addInfo = (event) => {
 	  event.preventDefault()
 	  const nameObject = {
-		  name: newName
+		  name: newName,
+		  number: newNumber
 	  }
 	  const result = checkDuplicateName()
 	  
 	  /* Add name to the existing phonebook only when the checkDuplicateName function
 	  returns false */
 	  if (!result) setPersons(persons.concat(nameObject))
+	  
 	  setNewName('')
+	  setNewNumber('')
   }
   
   const handleNameChange = (event) => {
@@ -44,13 +50,21 @@ const App = () => {
 	  setNewName(event.target.value)
   }
   
+  const handleNumberChange = (event) => {
+	  console.log(event.target.value)
+	  setNewNumber(event.target.value)
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addInfo}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
+		<div>
+		  number: <input value={newNumber} onChange={handleNumberChange}/>	
+		</div>
         <div>
           <button type="submit">add</button>
         </div>
@@ -58,7 +72,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul style={{ listStyle: 'none', padding: 0 }}>
 		  {persons.map(person => 
-			<Name key={person.name} name={person.name} />
+			<Name key={person.name} person={person} />
 		  )}
 	  </ul>
     </div>
