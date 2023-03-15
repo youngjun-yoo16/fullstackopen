3d46132ad3dee2600a79f54e7a8f8236e3cea557
youngjun-yoo16 <yoo156@purdue.edu>
2023-03-15 14:58:40 +0000

    Improved error message

--- example/src/App.js
+++ example/src/App.js
@@ -2,11 +2,23 @@ import { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+	  return null
+  }
+
+  return (
+	  <div className='error'>
+		  {message}
+	  </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-
+  const [errorMessage, setErrorMessage] = useState('some error happened...')	
   useEffect(() => {
 	noteService
 	  .getAll()
@@ -32,9 +44,13 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
 	  .catch(error => {
-		alert(
-		`the note '${note.content}' was already deleted from the server`
+		setErrorMessage(
+		`Note '${note.content}' was already removed from the server`
 		)
+		setTimeout(() => {
+			setErrorMessage(null)
+		}, 5000)
+		
 		/* The filter method returns a new array comprising only the items from the list 
 		for which the function that was passed as a parameter returns true. */
 		setNotes(notes.filter(note => note.id !== id))
@@ -77,6 +93,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+	  <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
