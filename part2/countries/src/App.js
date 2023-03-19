583a786f0134eeaf5e46bd9d8da00039fcf3855f
youngjun-yoo16 <yoo156@purdue.edu>
2023-03-19 02:02:00 +0000

    2.18*: Created UI and implemented the matching query logic

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,19 +1,39 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Countries = ({ filtered }) => {
+	if (typeof(filtered) === 'string') {
+		return <p>{filtered}</p>
+	} else {
+		return (
+			<ul style={{ listStyle: 'none', padding: 0 }}>
+				{filtered.map((country) => (
+					<li key={country.official}>
+						{country.common}
+					</li>
+				))}
+			</ul>
+		)
+	}
+}
+
 const App = () => {
 	const [query, setNewQuery] = useState('')
+	const [countries, setNewCountries] = useState([])
 	
 	useEffect(() => {
 		if (query.length > 0) {
 			console.log(`Search query is now ${query}`)
 			axios
-			  .get(`https://restcountries.com/v3.1/all?fields=name`)
+			  .get('https://restcountries.com/v3.1/all?fields=name')
 			  .then(response => {
 				const name = (response.data.map(obj => obj.name))
 				const filteredInfo = (name.filter(({ common }) => common.toLowerCase().match(query.toLowerCase().trim())))
-				console.log(filteredInfo)
-				if (filteredInfo.length > 10) console.log('Too many matches, specify another filter')
+				if (filteredInfo.length > 10) {
+					setNewCountries('Too many matches, specify another filter')
+				} else {
+					setNewCountries(filteredInfo)	
+				} 
 			})
 		}
 	}, [query])
@@ -23,6 +43,7 @@ const App = () => {
 	return (
 		<div>
 			find countries <input value={query} onChange={hanleQueryChange} />
+			<Countries filtered={countries} />
 		</div>
 	)
 }
