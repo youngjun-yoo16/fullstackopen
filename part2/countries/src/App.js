9783f1e01360b4c3187ace2b4c10779965fa1d58
youngjun-yoo16 <yoo156@purdue.edu>
2023-03-21 14:30:47 +0000

    Finished 2.19*: Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,12 +1,20 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+/* How does showing view for the country work by simply putting one line?
+1. I've made getData as a separate function out of useEffect.
+2. By clicking the show button, it calls getData function with the name of that country as an argument.
+3. getData function gets the data of that country from the REST Countries API.
+4. Sets 'countries' state with the data of that country. 
+5. The page gets re-rendered and calls Countries component again. 
+6. Since the length of an array is 1, it goes through the second if statement. 
+7. Calls ShowView component with the data of that country as an argument. 
+8. ShowView component displays the view of the country. */
 const Display = ({ country, getData }) => (
     <li>
 		{country.common}{' '}
 		<button onClick={() => getData(country.common)}>show</button>
 	</li>
-	//<ShowView data={} />
 )
 
 const ShowView = ({ data }) => (
