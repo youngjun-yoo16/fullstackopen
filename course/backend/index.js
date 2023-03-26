12a5514ec1d605d91721a8629353fb6ca54567d0
youngjun-yoo16 <yoo156@purdue.edu>
2023-03-26 14:53:39 +0000

    Simple web server

--- course/backend/index.js
+++ course/backend/index.js
@@ -1,1 +1,27 @@
-console.log('Hello World')
+const http = require('http')
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
+})
+
+const PORT = 3002
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
