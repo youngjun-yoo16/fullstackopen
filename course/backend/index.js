ce8d1329d0b989a18a42f6232ab3bb2805441630
youngjun-yoo16 <yoo156@purdue.edu>
2023-03-27 10:29:10 +0000

    Added routing

--- course/backend/index.js
+++ course/backend/index.js
@@ -23,6 +23,10 @@ app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
 app.get('/api/notes/:id', (request, response) => {
 	const id = Number(request.params.id)
 	const note = notes.find(note => note.id === id)
