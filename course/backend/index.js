4b2d0d1e086134ea552f67323fdca39d9dfca7d1
youngjun-yoo16 <yoo156@purdue.edu>
2023-03-27 10:21:29 +0000

    Fetching a single resource

--- course/backend/index.js
+++ course/backend/index.js
@@ -23,8 +23,14 @@ app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-	response.json(notes)
+app.get('/api/notes/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const note = notes.find(note => note.id === id)
+	if (note) {
+		response.json(note)
+	} else {
+		response.status(404).end()
+	}
 })
 
 const PORT = 3002
