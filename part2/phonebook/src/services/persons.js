590ed53f020ebd964a7ceec16841a1c93e52a46b
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-11 17:41:44 +0000

    Finished 3.9 phonebook backend step9

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://json-sever-dev.run.goorm.site/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
