48f824af086dbf3bc32996a052c9f7a7bb563933
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-11 17:40:57 +0000

    Finished 3.9 phonebook backend step9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('req', (req, res) => { 
 	if (req.method === "POST") return JSON.stringify(req.body)
@@ -102,7 +105,7 @@ app.post('/api/persons', (request, response) => {
 	response.json(persons)
 })
 
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
