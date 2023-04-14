598888b0cc3a13df2a5a1eaff464ccaa87fc726e
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-14 08:36:33 +0000

    Connecting the backend to a database

--- course/backend/index.js
+++ course/backend/index.js
@@ -1,10 +1,34 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const cors = require('cors')
+const dotenv = require("dotenv")
+const app = express()
 
+dotenv.config()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+
+const url =
+  `mongodb+srv://fullstack:${process.env.MONGO_PASSWORD}@fullstackopen.toxqsyf.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
@@ -29,7 +53,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+	Note.find({}).then(notes => {
+		response.json(notes)
+	})
 })
 
 app.get('/api/notes/:id', (request, response) => {
