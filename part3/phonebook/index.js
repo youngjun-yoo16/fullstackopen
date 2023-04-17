0399df9eac181e8c1f2b7306233a307c0f5de196
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-17 16:54:28 +0000

    Finished 3.19*: Phonebook database, step7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -57,7 +57,7 @@ app.delete('/api/persons/:id', (request, response) => {
 	.catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body
 	
 	if (!body.name) {
@@ -82,21 +82,22 @@ app.post('/api/persons', (request, response) => {
 		number: body.number,
 	})
 	
-	person.save().then(savedPerson => {
-		console.log(`Added ${savedPerson.name} number ${savedPerson.number} to phonebook`)
-		response.json(savedPerson)
-	})
+	person.save()
+		.then(savedPerson => {
+			console.log(`Added ${savedPerson.name} number ${savedPerson.number} to phonebook`)
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
-	const body = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+	const { name, number } = request.body
 	
-	const person = {
-		name: body.name,
-		number: body.number,
-	}
-	
-	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+	Person.findByIdAndUpdate(
+		request.params.id, 
+		{ name, number },
+		{ new: true, runValidators: true, context: 'query' }
+	)
 		.then(updatedPerson => {
 			response.json(updatedPerson)
 		})
@@ -108,7 +109,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+	return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
--- part3/phonebook/models/people.js
+++ part3/phonebook/models/people.js
@@ -15,8 +15,12 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+	name: {
+		type: String,
+		minLength: 3,
+		required: true
+	},	
+	number: String
 })
 
 personSchema.set('toJSON', {
