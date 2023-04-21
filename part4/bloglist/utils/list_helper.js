5cac630b0bf99bab9d1e78e7cd1f4c166e615573
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-21 14:35:37 +0000

    Finished 4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	if (blogs.length === 0) return 0
+	
+	const likes = blogs.map(blog => blog.likes)
+	const total = likes.reduce((sum, number) => sum + number, 0)
+	
+	return total
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes,
 }
