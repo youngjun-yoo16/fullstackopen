04114ac4e4144a03837555b1b0dadcc6907c0917
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-25 19:03:28 +0000

    Initializing the database before tests

---
+++ course/backend/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@fullstackopen.toxqsyf.mongodb.net/testNoteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'CSS is hard',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
---
+++ course/backend/tests/note_api.test.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+/* Initializing the database before every test with the beforeEach function.
+By doing this, we ensure that the database is in the same state before every test is run. */
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+  
+/* Technically speaking, in async- await you create a thread (task) 
+which waits for the result and the rest of your code continues working.
+In synchronous code everything will wait, causing your code to “hang”. */
+  expect(response.body).toHaveLength(initialNotes.length)
+})
+
+test('a specific note is within the returned notes', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContain(
+    'Browser can execute only JavaScript'
+  )
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
