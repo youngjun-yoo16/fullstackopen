e15169fb06ffc99e9c540fcdb900ed9020adeb80
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-26 05:59:38 +0000

    Eliminating the try-catch with express-async-errors library

--- course/backend/controllers/notes.js
+++ course/backend/controllers/notes.js
@@ -6,19 +6,16 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   const note = new Note({
@@ -30,12 +27,9 @@ notesRouter.post('/', (request, response, next) => {
   response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end() 
 })
 
 notesRouter.put('/:id', (request, response, next) => {
