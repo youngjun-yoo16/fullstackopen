09e2ca59f196a884bc79c449678aaa8d97b73467
youngjun-yoo16 <yoo156@purdue.edu>
2023-04-29 18:20:00 +0000

    Refactoring tests

--- course/backend/tests/note_api.test.js
+++ course/backend/tests/note_api.test.js
@@ -12,108 +12,133 @@ beforeEach(async () => {
   await Note.deleteMany({})
 	
 /* The noteObjects variable is assigned to an array of Mongoose objects that are created 
-with the Note constructor for each of the notes in the helper.initialNotes array. */
+with the Note constructor for each of the notes in the helper.initialNotes array. 
   const noteObjects = helper.initialNotes
     .map(note => new Note(note))
   const promiseArray = noteObjects.map(note => note.save())
 /* The Promise.all method can be used for transforming an array of promises into a single promise, 
 that will be fulfilled once every promise in the array passed to it as a parameter is resolved. 
 The last line of code await Promise.all(promiseArray) waits until every promise for saving a note is finished, 
-meaning that the database has been initialized.*/ 
-  await Promise.all(promiseArray)
+meaning that the database has been initialized.
+  await Promise.all(promiseArray) */
+  await Note.insertMany(helper.initialNotes)
 })
 
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('when there is initially some notes saved', () => {
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+	
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/notes')
+    /* Technically speaking, in async- await you create a thread (task) 
+    which waits for the result and the rest of your code continues working.
+    In synchronous code everything will wait, causing your code to “hang”. */
+    expect(response.body).toHaveLength(helper.initialNotes.length)
+  })
 
-test('all notes are returned', async () => {
-  const response = await api.get('/api/notes')
-  
-/* Technically speaking, in async- await you create a thread (task) 
-which waits for the result and the rest of your code continues working.
-In synchronous code everything will wait, causing your code to “hang”. */
-  expect(response.body).toHaveLength(helper.initialNotes.length)
+  test('a specific note is within the returned notes', async () => {
+    const response = await api.get('/api/notes')
+
+    const contents = response.body.map(r => r.content)
+
+    expect(contents).toContain(
+      'Browser can execute only JavaScript'
+    )
+  })
 })
 
-test('a specific note is within the returned notes', async () => {
-  const response = await api.get('/api/notes')
+describe('viewing a specific note', () => {
+  test('succeeds with a valid id', async () => {
+    const notesAtStart = await helper.notesInDb()
 
-  const contents = response.body.map(r => r.content)
-  expect(contents).toContain(
-    'Browser can execute only JavaScript'
-  )
+    const noteToView = notesAtStart[0]
+
+    const resultNote = await api
+      .get(`/api/notes/${noteToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(resultNote.body).toEqual(noteToView)
+  })
+	
+  test('fails with statuscode 404 if note does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId()
+
+    await api
+      .get(`/api/notes/${validNonexistingId}`)
+      .expect(404)
+  })
+	
+  test('fails with statuscode 400 if id is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
+
+    await api
+      .get(`/api/notes/${invalidId}`)
+      .expect(400)
+  })
 })
 
-test('a valid note can be added', async () => {
-  const newNote = {
-    content: 'async/await simplifies making async calls',
-    important: true,
-  }
+describe('addition of a new note', () => {
+  test('succeeds with valid data', async () => {
+    const newNote = {
+      content: 'async/await simplifies making async calls',
+      important: true,
+    }
 
-  await api
-    .post('/api/notes')
-    .send(newNote)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    await api
+      .post('/api/notes')
+      .send(newNote)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  const notesAtEnd = await helper.notesInDb()
-  expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
+    const notesAtEnd = await helper.notesInDb()
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
 
-  const contents = notesAtEnd.map(n => n.content)
-  expect(contents).toContain(
-    'async/await simplifies making async calls'
-  )
+    const contents = notesAtEnd.map(n => n.content)
+    expect(contents).toContain(
+      'async/await simplifies making async calls'
+    )
+  })
+
+  test('fails with status code 400 if data invalid', async () => {
+    const newNote = {
+      important: true
+    }
+
+    await api
+      .post('/api/notes')
+      .send(newNote)
+      .expect(400)
+
+    const notesAtEnd = await helper.notesInDb()
+
+    expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
+  })
 })
 
-test('note without content is not added', async () => {
-  const newNote = {
-    important: true
-  }
+describe('deletion of a note', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToDelete = notesAtStart[0]
 
-  await api
-    .post('/api/notes')
-    .send(newNote)
-    .expect(400)
+    await api
+      .delete(`/api/notes/${noteToDelete.id}`)
+      .expect(204)
 
-  const notesAtEnd = await helper.notesInDb()
+    const notesAtEnd = await helper.notesInDb()
 
-  expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
-})
+    expect(notesAtEnd).toHaveLength(
+      helper.initialNotes.length - 1
+    )
 
-test('a specific note can be viewed', async () => {
-  const notesAtStart = await helper.notesInDb()
+    const contents = notesAtEnd.map(r => r.content)
 
-  const noteToView = notesAtStart[0]
-
-  const resultNote = await api
-    .get(`/api/notes/${noteToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  expect(resultNote.body).toEqual(noteToView)
-})
-
-test('a note can be deleted', async () => {
-  const notesAtStart = await helper.notesInDb()
-  const noteToDelete = notesAtStart[0]
-
-  await api
-    .delete(`/api/notes/${noteToDelete.id}`)
-    .expect(204)
-
-  const notesAtEnd = await helper.notesInDb()
-
-  expect(notesAtEnd).toHaveLength(
-    helper.initialNotes.length - 1
-  )
-
-  const contents = notesAtEnd.map(r => r.content)
-
-  expect(contents).not.toContain(noteToDelete.content)
+    expect(contents).not.toContain(noteToDelete.content)
+  })
 })
 
 afterAll(async () => {
