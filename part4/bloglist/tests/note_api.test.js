c2ef957b224c5986697f5f7b42378ce9c5d85ded
youngjun-yoo16 <yoo156@purdue.edu>
2023-05-16 19:12:01 +0000

    4.23*: bloglist expansion, step11 in progress

--- part4/bloglist/tests/note_api.test.js
+++ part4/bloglist/tests/note_api.test.js
@@ -4,7 +4,7 @@ const helper = require('./test_helper')
 const bcrypt = require('bcrypt')
 const app = require('../app')
 const api = supertest(app)
-
+const jwt = require("jsonwebtoken");
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -33,64 +33,81 @@ test('blog posts have a unique identifier property named id', async () => {
   ids.forEach(id => expect(id).toBeDefined())
 })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'For the HTTP POST request test',
-    author: 'John Doe',
-	url: 'http://fullstackopen.com',
-	likes: 12
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/) 
-  
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+describe('addition of a new blog', () => {
+	let token = null
 	
-  const contents = blogsAtEnd.map(blog => blog.title)	
-  expect(contents).toContain(
-    'For the HTTP POST request test'
-  )
-})
+	beforeAll(async() => {
+	  await Blog.deleteMany({})	
+		
+      const passwordHash = await bcrypt.hash("abcde", 10);
+      const user = await new User({ username: "name", passwordHash }).save();
 
-test('missing likes property will default to the value 0', async () => {
-  const newBlog = {
-    title: 'Blog without the likes property',
-    author: 'Ada Lovelace',
-	url: 'http://dummytest.com'
-  }
-  
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+      const userForToken = { username: "name", id: user.id };
+      return (token = jwt.sign(userForToken, process.env.SECRET));
+	})
 	
-  const contents = blogsAtEnd.map(blog => blog.title)	
-  expect(contents).toContain(
-    'Blog without the likes property'
-  )
-})
+	  test('a valid blog can be added', async () => {
+	  const newBlog = {
+		title: 'For the HTTP POST request test',
+		author: 'John Doe',
+		url: 'http://fullstackopen.com',
+		likes: 12
+	  }
 
-test('missing title or url properties will respond with the status code 400', async () => {
-  const newBlog = {
-    author: 'Ada Lovelace',
-	likes: 23
-  }
-  
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+	  await api
+		.post('/api/blogs')
+		.set("Authorization", `Bearer ${token}`)	
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/) 
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+	  const blogsAtEnd = await helper.blogsInDb()
+	  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+	  const contents = blogsAtEnd.map(blog => blog.title)	
+	  expect(contents).toContain(
+		'For the HTTP POST request test'
+	  )
+	})
+
+	test('missing likes property will default to the value 0', async () => {
+	  const newBlog = {
+		title: 'Blog without the likes property',
+		author: 'Ada Lovelace',
+		url: 'http://dummytest.com'
+	  }
+
+	  await api
+		.post('/api/blogs')
+		.set("Authorization", `Bearer ${token}`)
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+	  const blogsAtEnd = await helper.blogsInDb()
+	  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+	  const contents = blogsAtEnd.map(blog => blog.title)	
+	  expect(contents).toContain(
+		'Blog without the likes property'
+	  )
+	})
+
+	test('missing title or url properties will respond with the status code 400', async () => {
+	  const newBlog = {
+		author: 'Ada Lovelace',
+		likes: 23
+	  }
+
+	  await api
+		.post('/api/blogs')
+		.set("Authorization", `Bearer ${token}`)
+		.send(newBlog)
+		.expect(400)
+
+	  const blogsAtEnd = await helper.blogsInDb()
+	  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+	})
 })
 
 describe('deletion of a blog', () => {
