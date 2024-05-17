20d7fc797c19df90899274fb73abc3e59f05db5c
youngjun-yoo16 <yoo156@purdue.edu>
2024-05-17 17:48:29 +0900

    b. Many reducers - Redux Toolkit

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,34 +1,25 @@
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
 
-import noteReducer from "./reducers/noteReducer";
+import noteReducer, {
+  createNote,
+  toggleImportanceOf,
+} from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer,
+  },
 });
-
-const store = createStore(reducer);
 
 console.log(store.getState());
 
-/* ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
-); */
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App/>
-  </Provider>
 );
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
