a4fd898861be1330fac9f11701dfeffccc3667aa
youngjun-yoo16 <yoo156@purdue.edu>
2024-05-25 00:14:04 +0900

    d. React Query, useReducer and the context - Synchronizing data to the server using React Query

--- part6/query-notes/src/requests.js
+++ part6/query-notes/src/requests.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+const baseUrl = "http://localhost:3001/notes";
+
 export const getNotes = () => {
   return axios.get("http://localhost:3001/notes").then((res) => res.data);
 };
+
+export const createNote = (newNote) =>
+  axios.post(baseUrl, newNote).then((res) => res.data);
+
+export const updateNote = (updatedNote) =>
+  axios
+    .put(`${baseUrl}/${updatedNote.id}`, updatedNote)
+    .then((res) => res.data);
