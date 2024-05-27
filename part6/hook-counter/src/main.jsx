ce351c03caf7bc07777f4faa4296dee32e32e79a
youngjun-yoo16 <yoo156@purdue.edu>
2024-05-27 23:23:52 +0900

    d. React Query, useReducer and the context - Defining the counter context in a separate file

--- part6/hook-counter/src/main.jsx
+++ part6/hook-counter/src/main.jsx
@@ -1,4 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { CounterContextProvider } from "./CounterContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
+);
