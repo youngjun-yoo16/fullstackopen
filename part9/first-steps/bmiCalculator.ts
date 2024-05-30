7a606a194f1749b2429f5bd495bacce471c24251
youngjun-yoo16 <yoo156@purdue.edu>
2024-05-30 20:33:39 +0900

    Finished 9.1 Body mass index

---
+++ part9/first-steps/bmiCalculator.ts
@@ -0,0 +1,48 @@
+interface BmiValues {
+  heightInCm: number;
+  weightInKg: number;
+}
+
+const parseArguments = (height: number, weight: number): BmiValues => {
+  if (!isNaN(height) && !isNaN(weight)) {
+    return {
+      heightInCm: height,
+      weightInKg: weight,
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+const calculateBmi = (heightInCm: number, weightInKg: number): string => {
+  const bmi = (weightInKg / (heightInCm * heightInCm)) * 10000;
+
+  if (bmi < 16) {
+    return "Underweight (Severe thinness)";
+  } else if (bmi >= 16 && bmi < 17) {
+    return "Underweight (Moderate thinness)";
+  } else if (bmi >= 17 && bmi < 18.5) {
+    return "Underweight (Mild thinness)";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight (Pre-obese)";
+  } else if (bmi >= 30 && bmi < 35) {
+    return "Obese (Class I)";
+  } else if (bmi >= 35 && bmi < 40) {
+    return "Obese (Class II)";
+  } else {
+    return "Obese (Class III)";
+  }
+};
+
+try {
+  const { heightInCm, weightInKg } = parseArguments(180, 74);
+  console.log(calculateBmi(heightInCm, weightInKg));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
