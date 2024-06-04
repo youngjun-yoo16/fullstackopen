8cc7f5355da691f524b64a966255d0bcaa4b1472
youngjun-yoo16 <yoo156@purdue.edu>
2024-06-05 02:02:41 +0900

    Finished 9.8: Patientor backend, step1

---
+++ part9/patientor-backend/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/ping", (_req, res) => {
+  console.log("Someone pinged here");
+  res.send("pong");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
